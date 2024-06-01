e87e4ee2f8adefbd8d3ea42e1028f86a6ce2d0aa
Andrew Hird <andrew.hird@palomagroup.com>
2024-06-01 14:14:05 +1200

    dynamically assign lazy load attribute

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ document.addEventListener("pointerdown", function () {
   audio.play();
   document.removeEventListener("click", arguments.callee);
 });
+
+const imageContainer = document.querySelector(".ravs");
+const images = imageContainer.querySelectorAll("img");
+for (let i = 2; i < images.length; i++) {
+  images[i].setAttribute("loading", "lazy");
+}
